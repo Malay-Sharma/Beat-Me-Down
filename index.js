e8b4db0c7c9d453ce495efa0429b83efad899c14
Malay-Sharma <malaysharma0001@gmail.com>
2025-01-14 20:56:46 +0530

    Health Bar Interaction

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
     }
     this.color = color
     this.isAttacking
+    this.health = 100
   }
 
   draw() {
@@ -154,7 +155,8 @@ function animate() {
     player.isAttacking
   ) {
     player.isAttacking = false
-    console.log('go')
+    enemy.health -= 20
+    document.querySelector('#enemyHealth').style.width = enemy.health + '%'
   }
   if (
     rectangularCollision({
@@ -164,7 +166,8 @@ function animate() {
     enemy.isAttacking
   ) {
     enemy.isAttacking = false
-    console.log('enemy attack successful')
+    player.health -= 20
+    document.querySelector('#playerHealth').style.width = player.health + '%'
   }
 }
 
