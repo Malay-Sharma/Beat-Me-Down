adc88a749a433ac1d9eeb3d30e2db42a577094d1
Malay-Sharma <malaysharma0001@gmail.com>
2025-01-15 21:25:23 +0530

    game timer and game over

--- index.js
+++ index.js
@@ -123,6 +123,33 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
   )
 }
+
+function determineWinner({ player, enemy, timerId }) {
+  clearTimeout(timerId)
+  document.querySelector('#displayText').style.display = 'flex'
+  if (player.health === enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Tie'
+  } else if (player.health > enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
+  } else if (player.health < enemy.health) {
+    document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+  }
+}
+let timer = 60
+let timerId
+function decreaseTimer() {
+  if (timer > 0) {
+    timerId = setTimeout(decreaseTimer, 1000)
+    timer--
+    document.querySelector('#timer').innerHTML = timer
+  }
+  if (timer === 0) {
+    determineWinner({ player, enemy, timerId })
+  }
+}
+
+decreaseTimer()
+
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = 'black'
@@ -169,7 +196,13 @@ function animate() {
     player.health -= 20
     document.querySelector('#playerHealth').style.width = player.health + '%'
   }
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+      determineWinner({ player, enemy, timerId })
+    }
 }
+
 
 animate()
 
